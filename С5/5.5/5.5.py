39165ea4e310273ac4db4ca059d344444a24c23c
Константин <toshnotik666@mail.ru>
2021-12-23 17:37:50 +0300

    Офис

--- С5/5.5/5.5.py
+++ С5/5.5/5.5.py
@@ -1,5 +1,5 @@
 import redis
-
+import json  # так-так-так, кто это тут у нас? Наш старый друг Джейсон заглянул на огонёк! Ну привет, чем ты сегодня нас порадуешь?
 
 red = redis.Redis(
     host='redis-16897.c273.us-east-1-2.ec2.cloud.redislabs.com',
@@ -7,3 +7,8 @@ red = redis.Redis(
     password='nMHi1YkfMrEHyXz3UvS9NyUOalM7RmB1'
 )
 
+dict1 = {'key1': 'value1', 'key2': 'value2'}  # создаём словарь для записи
+red.set('dict1', json.dumps(dict1))  # с помощью функции dumps() из модуля json превратим наш словарь в строчку
+converted_dict = json.loads(red.get('dict1'))  # с помощью знакомой нам функции превращаем данные, полученные из кэша обратно в словарь
+print(type(converted_dict))  # убеждаемся, что мы получили действительно словарь
+print(converted_dict)  # ну и выводим его содержание
