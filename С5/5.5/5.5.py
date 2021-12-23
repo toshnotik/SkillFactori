5f9dce2d90f8860d9a955beef8cf57355bb3566a
Константин <toshnotik666@mail.ru>
2021-12-23 17:44:36 +0300

    Офис

--- С5/5.5/5.5.py
+++ С5/5.5/5.5.py
@@ -12,3 +12,6 @@ red.set('dict1', json.dumps(dict1))  # с помощью функции dumps() 
 converted_dict = json.loads(red.get('dict1'))  # с помощью знакомой нам функции превращаем данные, полученные из кэша обратно в словарь
 print(type(converted_dict))  # убеждаемся, что мы получили действительно словарь
 print(converted_dict)  # ну и выводим его содержание
+
+red.delete('dict1')  # удаляются ключи с помощью метода .delete()
+print(red.get('dict1'))
